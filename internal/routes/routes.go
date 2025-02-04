dca5c1db93c8f680c0cd80232db7042f719baad1
Mohammad Reza Hashempour <mohammadhprp@gmail.com>
2025-02-04 18:41:20 +0330

    feat: create routes handler file

---
+++ internal/routes/routes.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mohammadhprp/zip-link/internal/handlers"
+)
+
+type RouteHandler struct {
+	app        *fiber.App
+	urlHandler *handlers.URLHandler
+}
+
+func NewRouteHandler(
+	app *fiber.App,
+	urlHandler *handlers.URLHandler,
+) *RouteHandler {
+	return &RouteHandler{
+		app:        app,
+		urlHandler: urlHandler,
+	}
+}
+
+func (h *RouteHandler) Setup() {
+	// Routes
+	h.app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber! 🚀")
+	})
+
+	// API group
+	api := h.app.Group("/api")
+
+	api.Get("/up", handlers.HealthCheck)
+
+	urls := api.Group("/urls")
+	urls.Post("/", h.urlHandler.Create)
+}
