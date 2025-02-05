fdcf14bdd8a04a8732206a55d9c0d942674eed57
Mohammad Reza Hashempour <mohammadhprp@gmail.com>
2025-02-05 10:58:47 +0330

    feat: add a getter to get client ip

--- internal/utils/helper.go
+++ internal/utils/helper.go
@@ -2,7 +2,11 @@ package utils
 
 import (
 	"log"
+	"net"
 	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func GetAppURL() string {
@@ -13,4 +17,26 @@ func GetAppURL() string {
 	}
 
 	return appURL
+}
+
+// GetClientIP extracts the real client IP address from the request
+func GetClientIP(c *fiber.Ctx) string {
+	if ip := c.Get("X-Forwarded-For"); ip != "" {
+		ips := strings.Split(ip, ",")
+		if len(ips) > 0 {
+			return strings.TrimSpace(ips[0])
+		}
+	}
+
+	// Check X-Real-IP header
+	if ip := c.Get("X-Real-IP"); ip != "" {
+		return ip
+	}
+
+	ipAddress := c.IP()
+	if net.ParseIP(ipAddress) == nil {
+		ipAddress = "unknown"
+	}
+
+	return ipAddress
 }
